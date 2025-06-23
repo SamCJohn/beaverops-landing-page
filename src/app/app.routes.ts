56c468b74da1f4db0539ec8f09ed46f37010935f
SamCJohn <samcjohnofficial@gmail.com>
2025-06-23 22:41:56 +0530

    Added the basic components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SectionComponent } from './section-component/section-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: SectionComponent }, 
+];
